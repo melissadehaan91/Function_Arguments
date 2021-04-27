f9901992bd26dea41c89a805aaa683bfb0465169
melissadehaan91 <80722664+melissadehaan91@users.noreply.github.com>
2021-04-27 11:13:36 +0200

    Add files via upload

---
+++ Function_Arguments/script.js
@@ -0,0 +1,9 @@
+const paintWall = function(side, color){
+    console.log('The ' + side + ' wall has been painted ' + color);
+}
+
+// paintWall('green');
+// paintWall('orange');
+
+// paintWall('north', 'orange');
+paintWall('south-east', 'grey');
